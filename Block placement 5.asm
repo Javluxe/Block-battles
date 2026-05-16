2c8fad8d7db8387f9e9ed710ea2e1434bd775e9a
fatimaasad4505 <fatimaasad4505@gmail.com>
2026-05-17 04:45:07 +0500

    Block placement (same error)

---
+++ Block placement 5.asm
@@ -0,0 +1,429 @@
+.model small
+.stack 100h
+
+.data
+
+    playerGrid  db 25 dup(0)
+
+    
+    inputRow    db 0
+    inputCol    db 0
+
+   
+    drawRow     db 0
+    drawCol     db 0
+    drawIdx     db 0
+    cellVal     db 0
+    screenRow   db 0
+    screenCol   db 0
+
+    blockCount  db 0
+
+    COL_WATER   equ 1Bh
+    COL_BLOCK   equ 1Eh
+    COL_HIT     equ 1Ch
+    COL_MISS    equ 1Fh
+    COL_LABEL   equ 1Eh
+    COL_HEADER  equ 1Bh
+
+    PGRID_ROW   equ 5
+    PGRID_COL   equ 4
+
+    placeTitle  db '   PLACE YOUR BLOCKS ON THE GRID   $'
+    placeInstr  db ' Enter row (1-5): $'
+    placeInstr2 db ' Enter col (1-5): $'
+    blockMsg    db ' Placing block $'
+    ofThree     db ' of 3          $'
+    errorRange  db ' Invalid! Enter 1-5. Try again.    $'
+    errorDupe   db ' Already placed there! Try again.  $'
+    successMsg  db ' Block placed!                     $'
+    doneMsg     db ' All 3 blocks placed! Press any key...$'
+    yourLabel   db '    YOUR GRID  $'
+    colNumbers  db '  1 2 3 4 5   $'
+    blankLine   db '                                   $'
+    statusLine  db ' =========================================$'
+
+.code
+
+SET_CURSOR MACRO row, col
+    mov ah, 02h
+    mov bh, 0
+    mov dh, row
+    mov dl, col
+    int 10h
+ENDM
+
+CLEAR_SCREEN MACRO
+    mov ah, 06h
+    mov al, 0
+    mov bh, 17h
+    mov ch, 0
+    mov cl, 0
+    mov dh, 24
+    mov dl, 79
+    int 10h
+    SET_CURSOR 0, 0
+ENDM
+
+HIDE_CURSOR MACRO
+    mov ah, 01h
+    mov ch, 20h
+    int 10h
+ENDM
+
+SHOW_CURSOR MACRO
+    mov ah, 01h
+    mov ch, 06h
+    mov cl, 07h
+    int 10h
+ENDM
+
+
+drawCell PROC
+    push ax
+    push bx
+    push cx
+    mov al, cellVal
+    cmp al, 0
+    je  dc_water
+    cmp al, 1
+    je  dc_block
+    cmp al, 2
+    je  dc_hit
+    mov ah, 09h
+    mov al, 'O'
+    mov bh, 0
+    mov bl, COL_MISS
+    mov cx, 1
+    int 10h
+    jmp dc_done
+    dc_water:
+        mov ah, 09h
+        mov al, '~'
+        mov bh, 0
+        mov bl, COL_WATER
+        mov cx, 1
+        int 10h
+        jmp dc_done
+    dc_block:
+        mov ah, 09h
+        mov al, 'B'
+        mov bh, 0
+        mov bl, COL_BLOCK
+        mov cx, 1
+        int 10h
+        jmp dc_done
+    dc_hit:
+        mov ah, 09h
+        mov al, 'X'
+        mov bh, 0
+        mov bl, COL_HIT
+        mov cx, 1
+        int 10h
+    dc_done:
+    pop cx
+    pop bx
+    pop ax
+    ret
+drawCell ENDP
+
+
+drawPlayerGrid PROC
+    push ax
+    push bx
+    push cx
+    push dx
+    push si
+
+    mov ah, 06h
+    mov al, 0
+    mov bh, COL_LABEL
+    mov ch, 3
+    mov cl, PGRID_COL
+    mov dh, 3
+    mov dl, PGRID_COL + 13
+    int 10h
+    SET_CURSOR 3, PGRID_COL
+    mov ah, 09h
+    lea dx, yourLabel
+    int 21h
+
+   
+    mov ah, 06h
+    mov al, 0
+    mov bh, COL_HEADER
+    mov ch, 4
+    mov cl, PGRID_COL
+    mov dh, 4
+    mov dl, PGRID_COL + 13
+    int 10h
+    SET_CURSOR 4, PGRID_COL
+    mov ah, 09h
+    lea dx, colNumbers
+    int 21h
+
+    lea si, playerGrid
+    mov drawRow, 0         
+
+    dg_rowloop:
+        cmp drawRow, 5
+        je  dg_done
+
+       
+        mov al, drawRow
+        add al, PGRID_ROW
+        mov screenRow, al
+        mov dh, screenRow
+        mov dl, PGRID_COL - 2
+        mov ah, 02h
+        mov bh, 0
+        int 10h
+        mov ah, 09h
+        mov al, drawRow
+        add al, '1'
+        mov bh, 0
+        mov bl, COL_HEADER
+        mov cx, 1
+        int 10h
+
+        mov drawCol, 0     
+
+        dg_colloop:
+            cmp drawCol, 5
+            je  dg_nextrow
+
+          
+            mov al, drawRow
+            mov bl, al
+            add al, bl      
+            add al, bl     
+            add al, bl      
+            add al, bl      
+            add al, drawCol 
+            mov drawIdx, al
+
+            mov bx, 0
+            mov bl, drawIdx
+            mov al, [si+bx]
+            mov cellVal, al
+
+           
+            mov al, drawRow
+            add al, PGRID_ROW
+            mov screenRow, al
+            mov al, drawCol
+            shl al, 1
+            add al, PGRID_COL
+            mov screenCol, al
+
+            mov ah, 02h
+            mov bh, 0
+            mov dh, screenRow
+            mov dl, screenCol
+            int 10h
+            call drawCell
+
+            inc drawCol
+            jmp dg_colloop
+
+        dg_nextrow:
+        inc drawRow
+        jmp dg_rowloop
+
+    dg_done:
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+drawPlayerGrid ENDP
+
+getValidInput PROC
+    push bx
+    push dx
+
+    gv_read:
+        mov ah, 01h
+        int 21h
+        cmp al, 13
+        je  gv_read
+        cmp al, 10
+        je  gv_read
+        cmp al, '1'
+        jl  gv_bad
+        cmp al, '5'
+        jg  gv_bad
+        jmp gv_good
+        gv_bad:
+            SET_CURSOR 17, 2
+            mov ah, 09h
+            lea dx, errorRange
+            int 21h
+            jmp gv_read
+    gv_good:
+        SET_CURSOR 17, 2
+        mov ah, 09h
+        lea dx, blankLine
+        int 21h
+        sub al, '1'        
+
+    pop dx
+    pop bx
+    ret
+getValidInput ENDP
+
+placeBlocks PROC
+    push ax
+    push bx
+    push dx
+    push si
+
+    mov blockCount, 1
+
+    pb_loop:
+        cmp blockCount, 4
+        je  pb_alldone
+
+       
+        SET_CURSOR 13, 2
+        mov ah, 09h
+        lea dx, blockMsg
+        int 21h
+        mov ah, 09h
+        mov al, blockCount
+        add al, '0'
+        mov bh, 0
+        mov bl, 1Eh
+        mov cx, 1
+        int 10h
+        SET_CURSOR 13, 18
+        mov ah, 09h
+        lea dx, ofThree
+        int 21h
+
+        
+        SET_CURSOR 15, 2
+        mov ah, 09h
+        lea dx, placeInstr
+        int 21h
+        SHOW_CURSOR
+        call getValidInput
+        mov inputRow, al
+
+        SET_CURSOR 16, 2
+        mov ah, 09h
+        lea dx, placeInstr2
+        int 21h
+        SHOW_CURSOR
+        call getValidInput
+        mov inputCol, al
+
+        mov al, inputRow    
+        mov bl, al          
+        add al, bl         
+        add al, bl          
+        add al, bl         
+        add al, bl          
+        add al, inputCol    
+        mov bx, 0
+        mov bl, al          
+
+        
+        lea si, playerGrid
+        mov al, [si+bx]     
+        cmp al, 1           
+        jne pb_place
+
+        SET_CURSOR 17, 2
+        mov ah, 09h
+        lea dx, errorDupe
+        int 21h
+        SET_CURSOR 15, 2
+        mov ah, 09h
+        lea dx, blankLine
+        int 21h
+        SET_CURSOR 16, 2
+        mov ah, 09h
+        lea dx, blankLine
+        int 21h
+        jmp pb_loop
+
+        pb_place:
+       
+        lea si, playerGrid
+        mov byte ptr [si+bx], 1     
+
+        
+        SET_CURSOR 17, 2
+        mov ah, 09h
+        lea dx, successMsg
+        int 21h
+
+        ; redraw grid
+        HIDE_CURSOR
+        call drawPlayerGrid     
+        SHOW_CURSOR
+
+        ; clear input lines
+        SET_CURSOR 15, 2
+        mov ah, 09h
+        lea dx, blankLine
+        int 21h
+        SET_CURSOR 16, 2
+        mov ah, 09h
+        lea dx, blankLine
+        int 21h
+
+        inc blockCount
+        jmp pb_loop
+
+    pb_alldone:
+    HIDE_CURSOR
+    SET_CURSOR 19, 2
+    mov ah, 09h
+    lea dx, doneMsg
+    int 21h
+    mov ah, 00h
+    int 16h
+
+    pop si
+    pop dx
+    pop bx
+    pop ax
+    ret
+placeBlocks ENDP
+
+main PROC
+    mov ax, @data
+    mov ds, ax
+
+    CLEAR_SCREEN
+    HIDE_CURSOR
+
+    mov ah, 06h
+    mov al, 0
+    mov bh, 1Eh
+    mov ch, 1
+    mov cl, 10
+    mov dh, 1
+    mov dl, 55
+    int 10h
+    SET_CURSOR 1, 10
+    mov ah, 09h
+    lea dx, placeTitle
+    int 21h
+
+    SET_CURSOR 20, 0
+    mov ah, 09h
+    lea dx, statusLine
+    int 21h
+
+    call drawPlayerGrid
+    call placeBlocks
+
+    mov ah, 4Ch
+    mov al, 0
+    int 21h
+main ENDP
+END main
